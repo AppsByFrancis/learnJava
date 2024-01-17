89d0068c6095b0aec3badbb985f6ebca3f1e0737
AppsByFrancis <majer.frantisek@hotmail.com>
2024-01-17 22:11:01 +0100

    multi-dimensional Array

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -2,7 +2,18 @@ package com.example.demo;
 
 public class Demo {
     public static void main(String [] args){
-        int nums [] = new int [4];
-        System.out.println(nums[3]);
+        int nums[][] = new int [3][4];
+
+        for(int i=0; i < 3; i++){
+            for(int j=0; j < 4; j++){
+                nums[i][j] = (int)(Math.random() * 100);
+            }
+        }
+        for(int i=0; i < 3; i++){ // i - rows
+            for(int j=0; j < 4; j++){ // j - columns
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
