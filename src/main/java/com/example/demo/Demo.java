842df7cf1ba5750575676d28c7cbd6a4918a123d
AppsByFrancis <majer.frantisek@hotmail.com>
2024-01-17 23:58:24 +0100

    array of objects

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -1,25 +1,36 @@
 package com.example.demo;
 
+class Student{
+    int rollno;
+    String name;
+    int marks;
+}
 public class Demo {
     public static void main(String [] args){
-        int nums[][] = new int [3][]; // jagged array - not sure if it will have certain amount of rows and columns
+        Student s1 = new Student();
+        s1.rollno = 1;
+        s1.name = "Joe";
+        s1.marks = 100;
 
-        nums[0] = new int[3]; //number of columns in first row
-        nums[1] = new int[4];
-        nums[2] = new int[5];
+        Student s2 = new Student();
+        s2.rollno = 1;
+        s2.name = "Michael";
+        s2.marks = 96;
 
-//        int nums[][][] = new int[3][4][3]; // 3D array
+        Student s3 = new Student();
+        s3.rollno = 3;
+        s3.name = "Frank";
+        s3.marks = 92;
 
-        for(int i=0; i < 3; i++){
-            for(int j=0; j < 4; j++){
-                nums[i][j] = (int)(Math.random() * 100);
-            }
+        Student students [] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for(int i=0; i < students.length; i++){
+            System.out.println(students[i].name);
         }
-//        for(int i=0; i < 3; i++){ // i - rows
-//            for(int j=0; j < 4; j++){ // j - columns
-//                System.out.print(nums[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+
+
     }
 }
