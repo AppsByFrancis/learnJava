a004cd94b7dc574b15a8d3f9d40d5b1dab7ed11a
AppsByFrancis <majer.frantisek@hotmail.com>
2024-01-20 18:55:32 +0100

    encapsulating and creating getters and setters

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -1,36 +1,32 @@
 package com.example.demo;
 
-class Student{
-    int rollno;
-    String name;
-    int marks;
+class Human {
+    private String name = "Joe";
+    private int age;
+
+    public String getName(){
+        return name;
+    }
+    public int getAge (){
+        return age;
+    }
+    public void setName(String a){
+        name = a;
+    }
+    public void setAge (int a){
+        age = a;
+    }
+
+
+
+
 }
 public class Demo {
-    public static void main(String [] args){
-        Student s1 = new Student();
-        s1.rollno = 1;
-        s1.name = "Joe";
-        s1.marks = 100;
-
-        Student s2 = new Student();
-        s2.rollno = 1;
-        s2.name = "Michael";
-        s2.marks = 96;
-
-        Student s3 = new Student();
-        s3.rollno = 3;
-        s3.name = "Frank";
-        s3.marks = 92;
-
-        Student students [] = new Student[3];
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
-
-        for(int i=0; i < students.length; i++){
-            System.out.println(students[i].name);
-        }
-
-
+    public static void main (String [] args){
+        Human joe = new Human();
+//        System.out.println(joe.getName());
+        joe.setAge(24);
+        joe.setName("Frank");
+        System.out.println(joe.getName() + " " + joe.getAge());
     }
 }
